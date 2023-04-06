eabf75364a919f1baac7acbe7aa2f652ea66bf19
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-04-05 18:07:30 -0600

    Sign up form and routing

---
+++ public/js/login.js
@@ -0,0 +1,59 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/blog');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/blog');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const loginForm = document.querySelector('.login-form');
+if (loginForm) {
+  loginForm.addEventListener('submit', loginFormHandler);
+} else {
+  // console.error('Login form not found');
+}
+
+const signupForm = document.querySelector('.signup-form');
+if (signupForm) {
+  signupForm.addEventListener('submit', signupFormHandler);
+} else {
+  // console.error('Signup form not found');
+}
