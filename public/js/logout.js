e34001701df9b530177629f2bae821a63057d771
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-04-05 20:24:46 -0600

    Login and logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,11 +1,12 @@
 const logout = async () => {
+  console.log("works");
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
   
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/movie');
     } else {
       alert(response.statusText);
     }
