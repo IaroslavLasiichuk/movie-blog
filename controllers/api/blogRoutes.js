cf6ea06e3a4a2887f4680877cb97fccb84aa07cc
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-04-06 20:33:51 -0600

    Working on blog section

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -3,7 +3,6 @@ const { Blog } = require('../../models');
 const { User } = require('../../models');
 const withAuth = require('../../utilis/auth');
 
-
 router.post('/', withAuth, async (req, res) => {
     try {
       const newPost = await Blog.create({
@@ -17,29 +16,4 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
   
-router.get('/', async (req, res) => {
-  try {
-    // Get all posts and JOIN with user data
-    const postData = await Blog.findAll({
-      // include: [
-      //   {
-      //     model: User,
-      //     attributes: ['name'],
-      //   },
-      // ],
-    });
-
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-    console.log(posts);
-    // Pass serialized data and session flag into template
-    res.render('blog', { 
-      posts, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,25 @@
-const seedBlogData = require('./seed-blog');
-
 const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
 
-const seedAll = async () => {
-  await sequelize.sync({ force: true});
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedBlogData();
-  console.log('\n----- BLOG CONTENT SEEDED -----\n');
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
   process.exit(0);
 };
 
-seedAll();
+seedDatabase();
