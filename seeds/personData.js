fc63e0469445c220bb6f7b2b111a38a67b152438
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-04-10 21:20:52 -0600

    Bio description added

--- seeds/personData.js
+++ seeds/personData.js
@@ -4,7 +4,7 @@ const personData = [
     {
       first_name: "Iaroslav",
       last_name: "Lasiichuk",
-      text: "Something about me.",
+      text: "Hi, everyone!I'm a young aspiring junior web developer with a passion for technology and a strong desire to make a difference in the world. I'm constantly seeking new challenges and opportunities to learn and grow as a developer",
       img: "/img/8L0A0474.jpg",
     },
     {
