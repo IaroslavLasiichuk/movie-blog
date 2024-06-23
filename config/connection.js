4ac1a24e8f491abf2d797e8a65980b9c7f8c57f8
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2024-06-22 20:53:50 -0600

    App migrated to AWS

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,16 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME_MOVIE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.RDS_ENDPOINT,
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
 module.exports = sequelize;
